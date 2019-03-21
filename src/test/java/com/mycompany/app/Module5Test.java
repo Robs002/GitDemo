7c1e1f58f33af9d0577b5bda528b90fe1b51887b
robin <singhisrobin@gmail.com>
2019-03-21 18:47:58 -0400

    first commit

---
+++ src/test/java/com/mycompany/app/Module5Test.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Module5Test {
+
+	@Test
+	public void Opener2() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Rohit");
+
+	}
+}
